490fd47e32b821d135853f6f30d8427b2ae75d69
Bam280 <andenboyo@gmail.com>
2023-10-19 04:02:45 +0700

    fe 0.1

---
+++ resources/views/frontend/index.blade.php
@@ -0,0 +1,4 @@
+@extends('frontend.partials.master')
+@section('content')
+    
+@endsection
---
+++ resources/views/frontend/partials/master.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+@include('frontend.partials.head')
+
+<body>
+       
+
+    @include('frontend.partials.navbar')
+
+    @yield('content')
+
+    @include('frontend.partials.footer')
+    @include('frontend.partials.script')
+    @yield('scripts')
+
+</body>
+</html>
