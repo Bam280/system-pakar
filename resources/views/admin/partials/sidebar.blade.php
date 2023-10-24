53acebce0f83d6e29c7730ae98e72ceefcf4cbb0
Bam280 <andenboyo@gmail.com>
2023-10-24 17:38:32 +0700

    add feature Login account

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -8,49 +8,11 @@
                      <i data-feather="pie-chart"></i>
                  </span>
                  <span class="sidebar-nav-name">
-                     Menu 1
-                 </span>
-                 <span class="sidebar-nav-end">
-                     <i data-feather="chevron-right" class="nav-collapse-icon"></i>
+                     Dashboard
                  </span>
              </a>
-
-             <ul class="sidebar-sub-nav collapse" id="example">
-                 <li class="sidebar-nav-item">
-                     <a href="" class="sidebar-nav-link">
-                         <span class="sidebar-nav-abbr">
-                             Mn
-                         </span>
-                         <span class="sidebar-nav-name">
-                             Sub Menu 1
-                         </span>
-                     </a>
-                 </li>
-
-                 <li class="sidebar-nav-item">
-                     <a href="" class="sidebar-nav-link">
-                         <span class="sidebar-nav-abbr">
-                             Mn
-                         </span>
-                         <span class="sidebar-nav-name">
-                             Sub Menu 2
-                         </span>
-                     </a>
-                 </li>
-                 <li class="sidebar-nav-item">
-                     <a href="" class="sidebar-nav-link">
-                         <span class="sidebar-nav-abbr">
-                             Mn
-                         </span>
-                         <span class="sidebar-nav-name">
-                             Sub Menu 3
-                         </span>
-                     </a>
-                 </li>
-             </ul>
          </li>
-
-         <li class="sidebar-nav-item">
+         {{-- <li class="sidebar-nav-item">
              <a class="sidebar-nav-link collapsed" data-toggle="collapse" href="#navTables" aria-expanded="false"
                  aria-controls="navTables">
                  <span class="sidebar-nav-icon">
@@ -92,7 +54,7 @@
                      </a>
                  </li>
              </ul>
-         </li>
+         </li> --}}
          <li class="sidebar-nav-item">
              <a class="sidebar-nav-link collapsed" data-toggle="collapse" href="#navForms" aria-expanded="false"
                  aria-controls="navForms">
@@ -100,70 +62,9 @@
                      <i data-feather="edit"></i>
                  </span>
                  <span class="sidebar-nav-name">
-                     Daftar Warga
-                 </span>
-                 <span class="sidebar-nav-end">
-                     <i data-feather="chevron-right" class="nav-collapse-icon"></i>
+                     Database Nilai
                  </span>
              </a>
-
-             <ul class="sidebar-sub-nav collapse" id="navForms">
-                 <li class="sidebar-nav-item">
-                     <a href="" class="sidebar-nav-link">
-                         <span class="sidebar-nav-abbr">
-                             Mw
-                         </span>
-                         <span class="sidebar-nav-name">
-                             Manage Warga
-                         </span>
-                     </a>
-                 </li>
-
-                 <li class="sidebar-nav-item">
-                     <a href="./layouts/form_advanced.html" class="sidebar-nav-link">
-                         <span class="sidebar-nav-abbr">
-                             Ma
-                         </span>
-                         <span class="sidebar-nav-name">
-                             Manage Application
-                         </span>
-                     </a>
-                 </li>
-             </ul>
          </li>
-         <li class="sidebar-nav-item">
-             <a class="sidebar-nav-link collapsed" data-toggle="collapse" href="#navTables2" aria-expanded="false"
-                 aria-controls="navTables2">
-                 <span class="sidebar-nav-icon">
-                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                         class="bi bi-person-workspace" viewBox="0 0 16 16">
-                         <path
-                             d="M4 16s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H4Zm4-5.95a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
-                         <path
-                             d="M2 1a2 2 0 0 0-2 2v9.5A1.5 1.5 0 0 0 1.5 14h.653a5.373 5.373 0 0 1 1.066-2H1V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v9h-2.219c.554.654.89 1.373 1.066 2h.653a1.5 1.5 0 0 0 1.5-1.5V3a2 2 0 0 0-2-2H2Z" />
-                     </svg>
-                 </span>
-                 <span class="sidebar-nav-name">
-                     Admin Service
-                 </span>
-                 <span class="sidebar-nav-end">
-                     <i data-feather="chevron-right" class="nav-collapse-icon"></i>
-                 </span>
-             </a>
-
-             <ul class="sidebar-sub-nav collapse" id="navTables2">
-                 <li class="sidebar-nav-item">
-                     <a href="" class="sidebar-nav-link">
-                         <span class="sidebar-nav-abbr">
-                             Ma
-                         </span>
-                         <span class="sidebar-nav-name">
-                             Manage Admin
-                         </span>
-                     </a>
-                 </li>
-             </ul>
-         </li>
-
      </ul>
  </div><!-- Sidebar End -->
