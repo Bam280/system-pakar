36058401e6fd11d23bb3c87c9880ff2947b3bf86
nurhudajoantama <nurhuda.joantama14@gmail.com>
2024-02-06 08:34:48 +0700

    add datatable action

---
+++ resources/views/components/datatable-action.blade.php
@@ -0,0 +1,15 @@
+@php
+    $editLink ??= '#';
+    $deleteLink ??= '#';
+@endphp
+
+<a href="{{ $editLink }}" class="btn btn-sm btn-primary">
+    <span class="oi oi-pencil"></span>
+</a>
+<form class="d-inline" action="{{ $deleteLink }}" method="post" onsubmit="return confirm('Apakah Serius Ingin Menghapus Data Ini?')">
+    @csrf
+    @method('delete')
+    <button type="submit" class="btn btn-sm btn-danger">
+        <span class="oi oi-trash"></span>
+    </button>
+</form>
